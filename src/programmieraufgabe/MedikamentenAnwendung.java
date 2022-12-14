d8cf77c172931235c9f394d4e9a4cd9730553dc8
Lukas248 <lukas.steegmaier@gmail.com>
2022-12-14 10:33:44 +0100

    Anlegen Methode erstellt

--- src/programmieraufgabe/MedikamentenAnwendung.java
+++ src/programmieraufgabe/MedikamentenAnwendung.java
@@ -1,1 +1,15 @@
+package programmieraufgabe;
+
+public class MedikamentenAnwendung {
+  
+    Medikamente medikament;
+
+    public void anlegen(String name, String kategorie, double preis, int anzahl) {
+
+        medikament = new Medikamente(name, kategorie, preis, anzahl);
+
+    }
+
+
+}
 
