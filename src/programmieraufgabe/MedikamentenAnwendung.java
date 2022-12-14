c768a8c7004c7be08b99eb05c83d1f953c3a3b23
Lukas248 <lukas.steegmaier@gmail.com>
2022-12-14 10:39:37 +0100

    anlegen bearbeitet

--- src/programmieraufgabe/MedikamentenAnwendung.java
+++ src/programmieraufgabe/MedikamentenAnwendung.java
@@ -1,5 +1,7 @@
 package programmieraufgabe;
 
+import java.io.*;
+
 public class MedikamentenAnwendung {
   
     Medikamente medikament;
@@ -7,6 +9,22 @@ public class MedikamentenAnwendung {
     public void anlegen(String name, String kategorie, double preis, int anzahl) {
 
         medikament = new Medikamente(name, kategorie, preis, anzahl);
+
+        try (InputStream eingabe = new FileInputStream(new File("./dateien/medikamente.txt"))) {
+
+            int zeichen;
+            while((zeichen = eingabe.read()) != -1) {
+
+                System.out.print(zeichen);
+
+            }
+
+        }
+        catch(IOException e) {
+
+
+
+        }
 
     }
 
